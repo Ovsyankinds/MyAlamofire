f03dbb2bc36f24e734c2f2f23fb79943eb950e04
Ovsyankinds <ovsyankinds@Ovsyankindss-iMac.local>
2017-08-09 17:19:25 +0400

    First query and parsing JSON for tableView

--- MyAlamofire/ViewController.swift
+++ MyAlamofire/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    var arr = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://swapi.co/api/people/1").responseJSON { response in
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+                print("------------")
+                
+                do{
+                let myJson = try JSONSerialization.jsonObject(with: response.data!, options: []) as! [String: AnyObject]
+                    
+                    
+                    self.arr.append((myJson["name"] as? String)!)
+                    self.arr.append((myJson["hair_color"] as? String)!)
+                    self.arr.append((myJson["eye_color"] as? String)!)
+                    self.arr.append(String((myJson["height"] as? String)!)!)
+                    self.arr.append(String((myJson["mass"] as? String)!)!)
+                    
+                    self.tableView.reloadData()
+                    
+                    //print(myJson["birth_year"]! as! String)
+                    
+                    print(self.arr)
+                } catch{
+                    print("Error")
+                }
+                
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+
+extension ViewController: UITableViewDataSource{
+    @available(iOS 2.0, *)
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arr.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel!.text = self.arr[indexPath.row] 
+        return cell
+    }
+    
+}
+
+
+
+
+
+
+
+
 
